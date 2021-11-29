80acf6ef863ac50733c58de0bcd6a64e7c6ce26f
Mathew Lee <matt_lee07@live.ca>
2021-11-29 02:03:50 -0500

    refactor to make a bit more readable

--- compatible.py
+++ compatible.py
@@ -17,6 +17,8 @@ from PIL import Image, ImageDraw
 
 def isCompatible(homeFootprint, lotBoundaries):
     matrix = constructMatrix(lotBoundaries)
+    # TODO: Implement setBack by changing 1s to 0s at border of poperty
+    # TODO: Cont: Assume that units are same measurement otherwise need to scale
     result = bruteForceAttempt(matrix, homeFootprint[0], homeFootprint[1])
     return result
 
@@ -55,40 +57,69 @@ def bruteForceAttempt(matrix, width, length):
     while i < rows:
         j = 0
         while j < columns:
-            print(matrix[i][j])
             countHorizontal = 0
             countVertical = 0
             if matrix[i][j] == 1:
-                x = i
-                y = j
-                while y < columns:
-                    if matrix[i][y] == 1:
-                        countHorizontal += 1
-                    else:
-                        break
-                    y += 1
-                while x < rows:
-                    if matrix[x][j] == 1:
-                        countVertical += 1
-                    else:
-                        break
-                    x += 1
+                countHorizontal = getConsecutiveOnesHorizontal(
+                    i, j, columns, countHorizontal, matrix
+                )
+                countVertical = getConsecutiveOnesVertical(
+                    i, j, rows, countVertical, matrix
+                )
 
             # Check if criteria met
-            if (countHorizontal >= width and countVertical >= length) or (
-                countHorizontal >= length & countVertical >= width
+            if (
+                countHorizontal != 0
+                and countVertical != 0
+                and isCriteriaMet(countHorizontal, countVertical, width, length)
             ):
                 return True
 
             # determine horizontal move
-            if countHorizontal == 0:
-                j += 1
-            elif countHorizontal - 1 >= min(width, length):
-                j += 1
-            else:
-                j += countHorizontal
+            j = determineHorizontalMovement(countHorizontal, width, length, j)
+
         i += 1
-    return False 
+    return False
+
+
+# this and function below are similar consider refactoring again.
+def getConsecutiveOnesHorizontal(x, y, columns, countHorizontal, matrix):
+    while y < columns:
+        if matrix[x][y] == 1:
+            countHorizontal += 1
+        else:
+            break
+        y += 1
+    return countHorizontal
+
+# CountVertical likely doesn't need to be passed in because it will always be 0.
+# Same comment applies to getConsecutiveOnesHorizontal function
+def getConsecutiveOnesVertical(x, y, rows, countVertical, matrix):
+    while x < rows:
+        if matrix[x][y] == 1:
+            countVertical += 1
+        else:
+            break
+        x += 1
+    return countVertical
+
+
+def determineHorizontalMovement(countHorizontal, width, length, index):
+    if (countHorizontal == 0) or (countHorizontal - 1 >= min(width, length)):
+        index += 1
+        return index
+
+    index += countHorizontal
+    return index
+
+
+def isCriteriaMet(countHorizontal, countVertical, width, length):
+    print("comparison made ", countHorizontal, countVertical, width, length)
+    if (countHorizontal >= width and countVertical >= length) or (
+        countHorizontal >= length and countVertical >= width
+    ):
+        return True
+    return False
 
 
 if __name__ == "__main__":
